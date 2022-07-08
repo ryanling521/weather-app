e6da490e4d3fa76655228efcb84bcc1aa2742fce
Ryan <ryanling521@g.ucla.edu>
2022-07-07 21:13:34 -0700

    added search bar functionality

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-// http://api.openweathermap.org/data/2.5/weather?q=London&APPID=479bc6949bbbf7c61ceba58032217558
 const body = document.body;
+const button = document.querySelector('#btn');
+const inputLocation = document.querySelector('#location');
 
 const cityName = document.createElement('div');
 body.append(cityName);
 
+const temperature = document.createElement('div');
+body.append(temperature);
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=479bc6949bbbf7c61ceba58032217558', {mode: 'cors'})
-.then(function(response) {
-    return response.json();
-    
-}).then(function(response) {
-    console.log(response);
 
-    cityName.textContent = response.name;
-    
+function city(input) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&APPID=479bc6949bbbf7c61ceba58032217558`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+        
+    }).then(function(response) {
+        console.log(response);
+        cityName.textContent = `City: ${response.name}`;
+        temperature.textContent = `Temperature (Fahrenheit): ${response.main.temp}`;
+    })
+
+}
+
+
+button.addEventListener('click', () => {
+    const location = inputLocation.value;
+    city(location);
 })
